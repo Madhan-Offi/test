007b86e574d24272ce982b9d69c29e1e8cc4c94f
msgpc <106960497+msgpc@users.noreply.github.com>
2022-06-07 15:21:14 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -5,12 +5,12 @@ import subprocess
 
 from telegram.ext import CommandHandler
 from telegram import ParseMode
-from bot.modules import scrape
 from bot import dispatcher, updater, botStartTime, IMAGE_URL, OWNER_ID, AUTHORIZED_CHATS
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.message_utils import *
 from .helper.telegram_helper.filters import CustomFilters
 from .modules import authorize, list
+from bot.modules import scrape
 
 
 def start(update, context):
