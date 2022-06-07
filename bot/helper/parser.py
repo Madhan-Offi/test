83634a3d4a009f11862059572995ea62cd4fe768
msgpc <106960497+msgpc@users.noreply.github.com>
2022-06-07 14:17:28 +0530

    gp,drop

--- bot/helper/parser.py
+++ bot/helper/parser.py
@@ -40,3 +40,29 @@ def get_gp_link(url: str):
         return res.json()['url'].replace('\/','/')
     except:
         return False
+
+
+def droplink_bypass(url):
+    client = requests.Session()
+    res = client.get(url)
+
+    ref = re.findall("action[ ]{0,}=[ ]{0,}['|\"](.*?)['|\"]", res.text)[0]
+
+    h = {'referer': ref}
+    res = client.get(url, headers=h)
+
+    bs4 = BeautifulSoup(res.content, 'lxml')
+    inputs = bs4.find_all('input')
+    data = { input.get('name'): input.get('value') for input in inputs }
+
+    h = {
+        'content-type': 'application/x-www-form-urlencoded',                        
+        'x-requested-with': 'XMLHttpRequest'
+    }
+    p = urlparse(url)
+    final_url = f'{p.scheme}://{p.netloc}/links/go'
+
+    time.sleep(3.1)
+    res = client.post(final_url, data=data, headers=h).json()
+
+    return res
