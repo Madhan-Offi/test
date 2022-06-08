fcaeadb01d760e66586ddcfa736ef5693494b387
msgpc <106960497+msgpc@users.noreply.github.com>
2022-06-08 09:43:27 +0530

    Scrape

--- bot/modules/scrape.py
+++ bot/modules/scrape.py
@@ -9,30 +9,30 @@ from bot.helper.parser import get_gp_link
 
 def scrape_gp(update, context):
     buttons = button_builder.ButtonMaker()
-    buttons.buildbutton("❤️‍🔥 JNS BOTS ❤️‍🔥", "https://t.me/JNS_BOTS")
-    buttons.buildbutton("🧲JNS LEECHS🧲", "https://t.me/JNS_MIRROR")
+    buttons.buildbutton("ᴘʀɪᴍᴇ ʙᴏᴛs", "https://t.me/Prime_BotZ")
+    buttons.buildbutton("sᴜᴘᴘᴏʀᴛ", "https://t.me/PrimexCloud")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(1))
     try:
        query = update.message.text.split()[1]
     except:
-       sendMarkup('<b>send a GPLinks along with this command 👀\n\n ❗️reply to the link wont wokrs❗️</b>\n👉🏻 command GpLink', context.bot, update, reply_markup)
+       sendMarkup('<b>sᴇɴᴅ ᴀ ɢᴘʟɪɴᴋs ᴀʟᴏɴɢ ᴡɪᴛʜ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ 👀</b>', context.bot, update, reply_markup)
        return
  
     if not query.startswith("https://gplinks") or query.startswith("gplinks"):
-       sendMessage('<b>Sorry 🤐 , <i>scrape only for GPLinks URLs. \nYou can use clone for GDrive, GdTot, AppDrive URLs</i> 🤠</b>', context.bot, update)
+       sendMessage('<b>sᴏʀʀʏ , <i>sᴄʀᴀᴘᴇ ᴏɴʟʏ ғᴏʀ ɢᴘʟɪɴᴋs. \nᴍᴏʀᴇ ʟɪɴᴋs ʙʏᴘᴀssɪɴɢ ᴀᴅᴅ sᴏᴏɴ..</i> 🤠</b>', context.bot, update)
        return
 
-    m = sendMessage('<b>Please wait a sec...🙇🏻  \nDont give another task 🙅 </b>', context.bot, update)
+    m = sendMessage('<b>ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ᴀ sᴇᴄ... \nᴅᴏɴ'ᴛ ɢɪᴠᴇ ᴀɴᴏᴛʜᴇʀ ᴛᴀsᴋ </b>', context.bot, update)
     link = get_gp_link(query)
     deleteMessage(context.bot, m)
     if not link:      
-       sendMessage("Something went wrong\nTry again later..🥺 ", context.bot, update)
+       sendMessage("sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ\nᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ..🥺 ", context.bot, update)
     else:
        buttons = button_builder.ButtonMaker()
-       buttons.buildbutton("✨ BYPASSED LINK ✨", link)
-       buttons.buildbutton("❤️‍🔥 JNS BOTS ❤️‍🔥", "https://t.me/JNS_BOTS")
+       buttons.buildbutton("ʙʏᴘᴀssᴇᴅ ʟɪɴᴋ", link)
+       buttons.buildbutton("ᴘʀɪᴍᴇ ʙᴏᴛs", "https://t.me/JNS_BOTS")
        reply_markup = InlineKeyboardMarkup(buttons.build_menu(1))
-       sendMarkup(f"<b>Thank you for using me 💫 \n\nHere is your direct link</b>", context.bot, update, reply_markup)
+       sendMarkup(f"<b>ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴅɪʀᴇᴄᴛ ʟɪɴᴋ 👇</b>", context.bot, update, reply_markup)
 
 gplink_handler = CommandHandler("scrape", scrape_gp,
                                filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
