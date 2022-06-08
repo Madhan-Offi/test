18f3801e3e2a6f85979451c0c3782ba011ca9101
msgpc <106960497+msgpc@users.noreply.github.com>
2022-06-08 10:25:56 +0530

    List

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -16,22 +16,22 @@ def list_drive(update, context):
         search = update.message.text.split(' ',maxsplit=1)[1]
         LOGGER.info(f"Searching: {search}")
         emoji = sendMessage('🧐', context.bot, update)
-        reply = sendMessage("𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠..... 𝐏𝐥𝐞𝐚𝐬𝐞 𝐰𝐚𝐢𝐭!\n\n 𝗜𝗳 𝗕𝗼𝗧 𝗱𝗼𝗲𝘀𝗻'𝘁 𝘀𝗲𝗻𝗱 𝗮𝗻𝘆, 𝗧𝗿𝘆 𝗮𝗴𝗮𝗶𝗻 𝘄𝗶𝘁𝗵 𝗠𝗼𝘃𝗶𝗲 𝗡𝗮𝗺𝗲 & 𝗬𝗲𝗮𝗿🙂.", context.bot, update)
+        reply = sendMessage("𝗦𝗲𝗮𝗿𝗰𝗵𝗶𝗻𝗴..... 𝗣𝗹𝗲𝗮𝘀𝗲 𝘄𝗮𝗶𝘁!\n\n 𝗜𝗳 𝗕𝗼𝗧 𝗱𝗼𝗲𝘀𝗻'𝘁 𝘀𝗲𝗻𝗱 𝗮𝗻𝘆, 𝗧𝗿𝘆 𝗮𝗴𝗮𝗶𝗻 𝘄𝗶𝘁𝗵 𝗠𝗼𝘃𝗶𝗲 𝗡𝗮𝗺𝗲 & 𝗬𝗲𝗮𝗿🙂.", context.bot, update)
         gdrive = GoogleDriveHelper(None)
         msg, button = gdrive.drive_list(search)
 
         if button:
             deleteMessage(context.bot, reply)
             deleteMessage(context.bot, emoji)
-            msgg = sendMessage("𝗟𝗶𝗻𝗸 𝗦𝗲𝗻𝗱𝗲𝗱 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗣𝗠 😎", context.bot, update)
+            msgg = sendMessage("𝗟𝗶𝗻𝗸 𝗦𝗲𝗻𝗱𝗲𝗱 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗣𝗠 😇", context.bot, update)
             sendPrivate(msg, context.bot, update, button)
         else:
-            editMessage(f'𝐍𝐨 𝐫𝐞𝐬𝐮𝐥𝐭 𝐟𝐨𝐮𝐧𝐝 𝐟𝐨𝐫 <code>{search}</code>', reply, button)
+            editMessage(f'𝗡𝗼 𝗿𝗲𝘀𝘂𝗹𝘁 𝗳𝗼𝘂𝗻𝗱 𝗳𝗼𝗿 <code>{search}</code>', reply, button)
             deleteMessage(context.bot, emoji)
 
     except IndexError:
         emo = sendMessage('😡', context.bot, update)
-        sendMessage("𝐃𝐨𝐧'𝐭 𝐮𝐬𝐞 𝐮𝐧𝐧𝐞𝐜𝐞𝐬𝐬𝐚𝐫𝐢𝐥𝐲, 𝐒𝐞𝐧𝐝 𝐚 𝐬𝐞𝐚𝐫𝐜𝐡 𝐤𝐞𝐲 𝐚𝐥𝐨𝐧𝐠 𝐰𝐢𝐭𝐡 𝐜𝐨𝐦𝐦𝐚𝐧𝐝", context.bot, update)
+        sendMessage("𝗗𝗼𝗻'𝘁 𝘂𝘀𝗲 𝘂𝗻𝗻𝗲𝗰𝗲𝘀𝘀𝗮𝗿𝗶𝗹𝘆, 𝗦𝗲𝗻𝗱 𝗮 𝘀𝗲𝗮𝗿𝗰𝗵 𝗸𝗲𝘆 𝗮𝗹𝗼𝗻𝗴 𝘄𝗶𝘁𝗵 𝗰𝗼𝗺𝗺𝗮𝗻𝗱", context.bot, update)
         deleteMessage(context.bot, emo)
 
 
